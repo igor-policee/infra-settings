2b5218d484d488c7c46709e4d435e2880fc8a906
Igor Polishchuk <igor.policee@gmail.com>
2022-11-19 22:05:07 +0300

    Update vmware-workstation-pro-16-install.sh

--- installation-scripts/ubuntu2004/vmware/vmware-workstation-pro-16-install.sh
+++ installation-scripts/ubuntu2004/vmware/vmware-workstation-pro-16-install.sh
@@ -10,17 +10,14 @@ if [ ! -x ${EXEC_FILE} ]; then
 
     if [ "${WHOAMI}" == 'root' ]; then
         DOWNLOAD_DIR='/root/Downloads'
-
-        if [ ! -d ${DOWNLOAD_DIR} ]; then
-            mkdir ${DOWNLOAD_DIR}
-        fi
         DOWNLOAD_SOURCE='https://filedn.com/lAv70NoRi0O8LpU8BzbIRXJ/software/VMware-Workstation-Full-16.2.4-20089737.x86_64.bundle'
         DOWNLOAD_FILE='VMware-Workstation-Full-16.2.4-20089737.x86_64.bundle'
         SERIAL_NUMBER='AZ3E8-DCD8J-0842Z-N6NZE-XPKYF'
 
         apt-get update &&
-            apt-get install -y build-essential linux-headers-"$(uname -r)"
-        cd ${DOWNLOAD_DIR} &&
+            apt-get install -y build-essential linux-headers-"$(uname -r)" &&
+            mkdir -p ${DOWNLOAD_DIR} &&
+            cd ${DOWNLOAD_DIR} &&
             wget "${DOWNLOAD_SOURCE}" &&
             chmod +x ${DOWNLOAD_FILE} &&
             sh ${DOWNLOAD_FILE} --console --set-setting vmware-workstation serialNumber ${SERIAL_NUMBER} &&
