ffa026997dfcf4192fca8c49820eeb13578df034
Igor Polishchuk <igor.policee@gmail.com>
2022-11-20 20:04:31 +0300

    Update nexus-install.sh

--- ubuntu2004/nexus-install.sh
+++ ubuntu2004/nexus-install.sh
@@ -9,13 +9,23 @@ if [ ! -x ${EXEC_FILE} ]; then
     WHOAMI=$(whoami)
 
     if [ "${WHOAMI}" == 'root' ]; then
-        # Install OpenJDK 1.8 on Ubuntu 20.04 LTS
-        apt install -y openjdk-8-jre-headless &&
-            mkdir -p /opt &&
-            cd /opt &&
-            wget https://download.sonatype.com/nexus/3/latest-unix.tar.gz &&
-            tar -zxvf latest-unix.tar.gz &&
-            mv /opt/nexus-* /opt/nexus
+        # Install Java on Ubuntu 20.04
+        apt update -y &&
+            apt install -y openjdk-8-jdk &&
+            # Create Dedicated Nexus System Account
+            useradd -M -d /opt/nexus -s /bin/bash -r nexus &&
+            echo "nexus   ALL=(ALL)       NOPASSWD: ALL" >/etc/sudoers.d/nexus &&
+            # Download Nexus Repository OSS Tarball
+            wget -O /opt/nexus-3.29.2-02-unix.tar.gz \
+                https://sonatype-download.global.ssl.fastly.net/repository/downloads-prod-group/3/nexus-3.29.2-02-unix.tar.gz &&
+            # Install Nexus Repository Manager on Ubuntu 20.04
+            mkdir -p /opt/nexus &&
+            tar xzf /opt/nexus-3.29.2-02-unix.tar.gz -C /opt/nexus/ --strip-components=1 &&
+            rm -f /opt/nexus-3.29.2-02-unix.tar.gz &&
+            # Set the proper ownership of the nexus directory
+            chown -R nexus: /opt/nexus &&
+            # Add user configuration in /opt/nexus/bin/nexus.rc
+            sed -i 's/#run_as_user=""/run_as_user="nexus"/' /opt/nexus/bin/nexus.rc
 
     else
 
