12be7083c315d7fc8bc68740beb4672809a7c23c
Igor Polishchuk <igor.policee@gmail.com>
2022-11-23 17:52:11 +0300

    Update

--- ubuntu2004/proxy.sh
+++ ubuntu2004/proxy.sh
@@ -2,9 +2,9 @@
 
 # Enabling / Disabling proxy
 
-if [ ${#} -eq 1 ]; then
+if [[ ${#} -eq 1 ]]; then
     STATE=${1}
-    if [ ${STATE} == 'on' ] && [ ${PROXY_STATE} != ${STATE} ]; then
+    if [[ ${STATE} == 'on' ]] && [[ ${STATE} != ${PROXY_STATE} ]]; then
         PROXY_URL="http://user81261:qdvs7j@185.198.153.134:3665"
         echo http_proxy=\"${PROXY_URL}\" >>/etc/environment
         echo https_proxy=\"${PROXY_URL}\" >>/etc/environment
@@ -13,16 +13,15 @@ if [ ${#} -eq 1 ]; then
         echo no_proxy=\"localhost,127.0.0.1,localaddress,.localdomain.com\" >>/etc/environment
         echo "Acquire::http::Proxy \"$(echo ${PROXY_URL})\";" >>/etc/apt/apt.conf
         echo "Acquire::https::Proxy \"$(echo ${PROXY_URL})\";" >>/etc/apt/apt.conf
-        echo PROXY_STATE=\"${STATE}\" >>/etc/environment
 
-    elif [ ${STATE} == 'on' ] && [ ${PROXY_STATE} == ${STATE} ]; then
+        echo PROXY_STATE=\"${STATE}\" >>/etc/environment
+        source /etc/environment
+
+    elif [[ ${STATE} == 'on' ]] && [[ ${STATE} == ${PROXY_STATE} ]]; then
         echo 'The proxy is already activated'
         exit 1
 
-    elif
-
-        [ ${STATE} == 'off' ]
-    then
+    elif [[ ${STATE} == 'off' ]]; then
         sed -i '/http_proxy/d' /etc/environment
         sed -i '/https_proxy/d' /etc/environment
         sed -i '/dns_proxy/d' /etc/environment
@@ -30,6 +29,8 @@ if [ ${#} -eq 1 ]; then
         sed -i '/no_proxy/d' /etc/environment
         sed -i '/Proxy/d' /etc/apt/apt.conf
         sed -i '/PROXY_STATE/d' /etc/environment
+
+        source /etc/environment
     else
         echo 'Invalid argument. Possible values: on/off'
         exit 1
